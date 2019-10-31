1c6cf42e25799492a1e4e608afd91684d6c83964
Abraham Moreno <adiaz2821@gmail.com>
2019-10-31 14:57:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(question)
   if question = question.downcase
-    puts 
+    return "HUH?! SPEAK UP, SONNY!"
     
-  
+  elsif question = question.upcase
+    return "NO, NOT SINCE 1938!"
+    
+  else
+    return "I LOVE YOU TOO PUMPKIN!"
 end 
