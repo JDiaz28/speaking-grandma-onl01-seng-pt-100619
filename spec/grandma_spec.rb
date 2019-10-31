7b60e5c1976c2d91471b5884767227aeeb6d5391
Abraham Moreno <adiaz2821@gmail.com>
2019-10-31 15:02:01 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -5,7 +5,7 @@ describe '#speak_to_grandma' do
 
   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
     expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
-  end
+  endhttps://www.platinumed.com/
 
   it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
     expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
